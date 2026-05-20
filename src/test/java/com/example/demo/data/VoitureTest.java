e8a990867cb56ab52723d3166af2ce1470dd3249
1-HugoRibeiro <ribeiroa4829@gmail.com>
2026-05-20 11:35:00 +0200

    test voiture java v3

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -8,12 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class VoitureTest {
+    Voiture v1;
+    Voiture v2
+
+    @BeforeAll{
+        v1 = new Voiture("Lamborghini", 250000);
+        v1.setId(1);
+        v2 = new Voiture("Renault",25000);
+        v2.setId(2);
+    }
 
     @Test
     void creerVoiture(){
         assertEquals(1,1);
-        /*assert java
-        pour plus*/
+        assert.isTrue(v1.getMarque().equals("Lamborghini"), v1.getPrix() == 250000);
+        assert.isFalse(v2.getMarque().notEquals("BMW"), v2.getPrix() == 110);
+        /*assert java v2*/
     }
 
 }
