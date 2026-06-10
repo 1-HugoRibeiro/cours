0ccd20f7a23317e7fae371d1c3c236dd0e1f2688
1-HugoRibeiro <ribeiroa4829@gmail.com>
2026-06-10 10:32:33 +0200

    allcar

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -11,7 +11,17 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+    public void testStatistique(){
+        StatistiqueImpl statistique = new StistiqueImpl();
+        Voiture v1 = new Voiture("Lamborghini",250000);
+        Voiture v2 = new Voiture("Ferrari",150000);
+        statistique.ajouter(v1);
+        statistique.ajouter(v2);
+        Echantillon echantillon = statistique.getPrixMoyen();
+
+        assertEquals(200000, stistique.prixMoyen().getPrix());
+        assertEquals(2, statistique.prixMoyen().getNombreDeVoitures());
+    }
 
 }
--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -27,4 +27,44 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Testvoid
+    void statistiquesVoitureAccessible() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 200000));
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2))
+                .andExpect(jsonPath("$.prixMoyen").value(200000));
+    }
+
+    @Test
+    void aucuneVoiture() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenThrow(ArithmeticException.class);
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void postVoitureFonctionne() throws Exception {
+        String v1Json = "{\"marque\":\"Ferrari\",\"prix\":150000}";
+
+        mockMvc.perform(post("/v1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(voitureJson))
+                .andExpect(status().isOk());
+
+        verify(statistiqueImpl, times(1)).ajouter(any(Voiture.class));
+    }
+
+    @Test
+    void postVoitureFonctionne2() throws Exception {
+        String v2Json = "{\"marque\":\"Lamborghini\",\"prix\":250000}";
+        
+        mockMvc.perform(post("/v2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(v2Json))
+            .andExpect(status().isOk());
+    }
+
 }
