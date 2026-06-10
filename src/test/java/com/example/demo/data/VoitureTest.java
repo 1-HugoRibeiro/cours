a67786202d583230e7d1d1b72881cdc3b7ce3899
1-HugoRibeiro <ribeiroa4829@gmail.com>
2026-06-10 11:03:04 +0200

    carService

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -11,8 +11,16 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
-        /*assert java*/
+        Voiture v1 = new Voiture("Lamborghini", 250000);
+        v1.setPrix(300000);
+        assertEquals(300000,v1.getPrix());
+
+        Voiture v2 = new Voiture("Renault",25000);
+        v2.setPrix(22500);
+        assertEquals(22500,v2.getPrix());
+
+        System.out.println(v1);
+        System.out.println(v2);
     }
 
 }
