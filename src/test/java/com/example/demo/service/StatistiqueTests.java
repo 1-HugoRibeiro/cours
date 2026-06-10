f8eaa0a954dc3b53a8347f5849f1b6806ef83dc4
1-HugoRibeiro <ribeiroa4829@gmail.com>
2026-06-10 10:47:50 +0200

    car

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -11,7 +11,17 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+    void TestStat(){
+        StatistiqueImpl stat = new StatistiqueImpl();
+        Voiture v1 = new Voiture("Lamborghini",250000);
+        Voiture v2 = new Voiture("Ferrari",150000);
+        stat.ajouter(v1);
+        stat.ajouter(v2);
+        Echantillon ech = stat.getPrixMoyen();
+
+        assertEquals(200000, ech.getPrixMoyen());
+        assertEquals(2, ech.getNombreDeVoitures());
+    }
 
 }
