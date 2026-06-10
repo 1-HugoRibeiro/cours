c47e7dedc8cdc7281909a9d84b5a6d0241dab98f
1-HugoRibeiro <ribeiroa4829@gmail.com>
2026-06-10 11:22:27 +0200

    service2

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,13 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.example.demo.service.StatistiqueImpl;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+    void testStatistique(){
+        StatistiqueImpl stat = new StatistiqueImpl();
+        Voiture v1 = new Voiture("lambo",15000);
+        Voiture v2 = new Voiture("Fefe",15000);
+
+        stat.ajouter(v1);
+        stat.ajouter(v2);
+
+        Echantillon ech = stat.prixMoyen();
+
+        assertEquals(15000,ech.getPrixMoyen());
+        assertEquals(2,ech.getNombreDeVoitures());
+
+    }
 
 }
